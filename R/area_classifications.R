c33fd2051e25172c9afd4b2d5d9ab038a133b5d9
Malcolm Morgan <mem48@users.noreply.github.com>
2022-07-07 16:50:24 +0100

    work on report

--- R/area_classifications.R
+++ R/area_classifications.R
@@ -2,6 +2,10 @@
 library(dplyr)
 library(ggplot2)
 
+all_long = readRDS("data/LSOA_cars_per_person_2002_2018_long.Rds")
+high_cpp = all_long[all_long$cars_per_person > 2,]
+high_cpp = unique(high_cpp$code)
+
 oac <- read.csv("D:/OneDrive - University of Leeds/Data/OA Bounadries/GB_OA_LSOA_MSOA_LAD_Classifications_2017.csv")
 
 oac <- oac[,c("LSOA11CD","SOAC11NM")]
@@ -15,23 +19,104 @@ table(substr(ru$LSOA11CD,1,1))
 
 ru <- left_join(ru, oac, by = "LSOA11CD")
 
-clasif = readRDS("data/lsoa_carpp_pop_classifications.Rds")
-clasif = clasif[,c("code","gradient_cars","pop_change_class","cars_change_class","desc_pop","desc_cars")]
+all <- left_join(all_long, ru, by = c("code" = "LSOA11CD"))
 
-ru <- left_join(ru, clasif, by = c("LSOA11CD" = "code"))
-
-casestudy <- read.csv("data/case_study_locations2.csv")
-
-all <- left_join(ru, casestudy, by = c("LSOA11CD" = "geo_code"))
-
-all$RUC11 <- factor(all$RUC11, levels = c("Urban major conurbation",
-                                          "Urban minor conurbation",
-                                          "Urban city and town",
-                                          "Urban city and town in a sparse setting",
+all$RUC11 <- factor(all$RUC11, levels = c("Rural village and dispersed",
+                                          "Rural village and dispersed in a sparse setting",
                                           "Rural town and fringe",
                                           "Rural town and fringe in a sparse setting",
-                                          "Rural village and dispersed",
-                                          "Rural village and dispersed in a sparse setting"))
+                                          "Urban city and town in a sparse setting",
+                                          "Urban city and town",
+                                          "Urban minor conurbation",
+                                          "Urban major conurbation"))
+
+
+
+all_summary_ru <- all %>%
+  filter(cars_per_person < 2) %>%
+  group_by(RUC11, year) %>%
+  summarise(median = median(cars_per_person, na.rm = TRUE),
+            mean = mean(cars_per_person, na.rm = TRUE),
+            q5 = quantile(cars_per_person, probs = 0.05, na.rm = TRUE),
+            q95 = quantile(cars_per_person, probs = 0.95, na.rm = TRUE),
+            q25 = quantile(cars_per_person, probs = 0.25, na.rm = TRUE),
+            q75 = quantile(cars_per_person, probs = 0.75, na.rm = TRUE),
+            min = min(cars_per_person, na.rm = TRUE),
+            max = max(cars_per_person, na.rm = TRUE),
+            sd = sd(cars_per_person, na.rm = TRUE))
+
+ggplot(all_summary_ru, aes(x = year, y = median, color = RUC11)) +
+  geom_line(size=2) +
+  ylab("Median cars per person") +
+  xlab("Year") +
+  guides(color=guide_legend(title="Rural/urban classification"))
+
+ggsave("plots/rural_urban.png", width = 210, height = 297, units = "mm")
+
+
+all_summary_oac <- all %>%
+  filter(cars_per_person < 2) %>%
+  group_by(SOAC11NM, year) %>%
+  summarise(median = median(cars_per_person, na.rm = TRUE),
+            mean = mean(cars_per_person, na.rm = TRUE),
+            q5 = quantile(cars_per_person, probs = 0.05, na.rm = TRUE),
+            q95 = quantile(cars_per_person, probs = 0.95, na.rm = TRUE),
+            q25 = quantile(cars_per_person, probs = 0.25, na.rm = TRUE),
+            q75 = quantile(cars_per_person, probs = 0.75, na.rm = TRUE),
+            min = min(cars_per_person, na.rm = TRUE),
+            max = max(cars_per_person, na.rm = TRUE),
+            sd = sd(cars_per_person, na.rm = TRUE))
+
+
+cols = c("Cosmopolitan student neighbourhoods" ='#955123',
+         "Ageing rural neighbourhoods" ='#007f42',
+         "Prospering countryside life" ='#3ea456',
+         "Remoter communities" ='#8aca8e',
+         "Rural traits" ='#cfe8d1',
+         "Achieving neighbourhoods" ='#00498d',
+         "Asian traits" ='#2967ad',
+         "Highly qualified professionals" ='#7b99c7',
+         "Households in terraces and flats" ='#b9c8e1',
+         "Challenged white communities" ='#e3ac20',
+         "Constrained renters" ='#edca1a',
+         "Hampered neighbourhoods" ='#f6e896',
+         "Hard-pressed flat dwellers" ='#fcf5d8',
+         "Ageing urban communities" ='#e64c2b',
+         "Aspiring urban households" ='#ec773c',
+         "Comfortable neighbourhoods" ='#faa460',
+         "Endeavouring social renters" ='#fcc9a0',
+         "Primary sector workers" ='#fee4ce',
+         "Inner city cosmopolitan" = '#f79ff0',
+         "Urban cultural mix" ='#6a339a',
+         "Young ethnic communities" ='#9f84bd',
+         "Affluent communities" ='#576362',
+         "Ageing suburbanites" ='#a1a2a1',
+         "Comfortable suburbia" ='#e5e4e3')
+
+ggplot(all_summary_oac, aes(x = year, y = median, color = SOAC11NM)) +
+  geom_line(size=2) +
+  ylab("Median cars per person") +
+  xlab("Year") +
+  guides(color=guide_legend(title="Area classification", ncol =1)) +
+  scale_color_manual(values=cols)
+
+ggsave("plots/OAC_class.png", width = 210, height = 297, units = "mm")
+
+
+
+  # geom_errorbar(aes(ymin=q25, ymax=q75), width=.2, # error bars are too wide
+  #               position=position_dodge(.9))
+
+# clasif = readRDS("data/lsoa_carpp_pop_classifications.Rds")
+# clasif = clasif[,c("code","gradient_cars","pop_change_class","cars_change_class","desc_pop","desc_cars")]
+# 
+# ru <- left_join(ru, clasif, by = c("LSOA11CD" = "code"))
+# 
+# casestudy <- read.csv("data/case_study_locations2.csv")
+# 
+# all <- left_join(ru, casestudy, by = c("LSOA11CD" = "geo_code"))
+
+
 
 all$class <- factor(all$class, levels = c(NA,
                                           "rising population falling cars",
--- R/change_near_new_stations.R
+++ R/change_near_new_stations.R
@@ -4,8 +4,12 @@ library(ggplot2)
 library(dplyr)
 
 all = readRDS("data/LSOA_cars_per_person_2002_2018_long.Rds")
+classif = readRDS("data/lsoa_carpp_pop_classifications.Rds")
+
 bounds = read_sf("data/LSOA_bounds_simplified.gpkg")
 stops = read_sf("data/new_stations.geojson")
+stops$cluster[is.na(stops$cluster)] <- "Other"
+stops$cluster_year <- paste0(stops$cluster," - ",stops$year)
 
 bounds = st_transform(bounds, 27700)
 stops = st_transform(stops, 27700)
@@ -16,23 +20,136 @@ bounds = st_join(bounds, buff)
 bounds = bounds[order(bounds$year),]
 bounds = bounds[!duplicated(bounds$geo_code),]
 bounds = bounds[bounds$geo_code %in% unique(all$code),]
-#qtm(bounds, fill = "year")
+#qtm(bounds[!is.na(bounds$year),], fill = "year") + qtm(stops)
+
+stop_join <- st_drop_geometry(bounds)
+names(stop_join) <- c("code","stop","year_stop","cluster","cluster_year")
+all <- left_join(all, stop_join, by = "code")
+all$cluster_year[is.na(all$cluster_year)] <- "None"
+all$cluster[is.na(all$cluster)] <- "None"
+all$year_offset = all$year - all$year_stop
 
 
-# Example E01033707
+all_summary_stop <- all %>%
+  filter(cars_per_person < 2) %>%
+  filter(cluster != "Other") %>%
+  filter(cluster != "None") %>%
+  group_by(cluster_year, year_offset) %>%
+  summarise(median = median(cars_per_person, na.rm = TRUE),
+            mean = mean(cars_per_person, na.rm = TRUE),
+            q5 = quantile(cars_per_person, probs = 0.05, na.rm = TRUE),
+            q95 = quantile(cars_per_person, probs = 0.95, na.rm = TRUE),
+            q25 = quantile(cars_per_person, probs = 0.25, na.rm = TRUE),
+            q75 = quantile(cars_per_person, probs = 0.75, na.rm = TRUE),
+            min = min(cars_per_person, na.rm = TRUE),
+            max = max(cars_per_person, na.rm = TRUE),
+            sd = sd(cars_per_person, na.rm = TRUE))
 
-all_sub = all[all$code == "E01033707",]
-all_sub$intervention_year = 2010
+#all_summary_stop$year_stop <- as.character(all_summary_stop$year_stop)
+
+cols = c("DLR Woolwich Extension - 2005" = "#cb181d",
+         "DLR Woolwich Extension - 2009" = "#fb6a4a",
+         "DLR Stratford Extension - 2011" = "#fcae91",
+         "East London Line - 2010" = "#ff7f00",
+         "Ebbe Vale Line - 2008" = "#238b45",
+         "Ebbe Vale Line - 2014" = "#74c476",
+         "Ebbe Vale Line - 2015" = "#bae4b3",
+         "Manchester Tram South - 2011" = "#7a0177",
+         "Manchester Tram Didsbury Line - 2013" = "#c51b8a",
+         "Manchester Tram East Line - 2013" = "#f768a1",
+         "Manchester Tram Airport Line - 2014" = "#fbb4b9",
+         "Manchester Tram Rochdale Line - 2012" = "#ce1256",
+         "Manchester Tram Rochdale Line - 2013" = "#df65b0",
+         "Manchester Tram Rochdale Line - 2014" = "#d7b5d8",
+         "Nottingham Tram North - 2004" = "#2b8cbe",
+         "Nottingham Tram South - 2015" = "#a6bddb")
+
+all_summary_stop$cluster_year <- factor(all_summary_stop$cluster_year, levels = names(cols))
+
+
+
+ggplot(all_summary_stop, aes(x = year_offset, y = median, color = cluster_year)) +
+  geom_vline(xintercept = 0) +
+  geom_line(size=1) +
+  scale_color_manual(values=cols) +
+  geom_smooth(method=lm, se=FALSE, linetype="dotted") +
+  ylab("Median cars per person") +
+  xlab("Year's since station(s) opended") +
+  guides(color=guide_legend(title="Project - year opened", ncol =1))
+  
+ggsave("plots/change_stations.png", width = 210, height = 297, units = "mm")
+
+
+all_summary_stop2 <- all %>%
+  filter(cars_per_person < 2) %>%
+  filter(cluster == "Other") %>%
+  group_by(stop, year_offset) %>%
+  summarise(median = median(cars_per_person, na.rm = TRUE),
+            mean = mean(cars_per_person, na.rm = TRUE),
+            q5 = quantile(cars_per_person, probs = 0.05, na.rm = TRUE),
+            q95 = quantile(cars_per_person, probs = 0.95, na.rm = TRUE),
+            q25 = quantile(cars_per_person, probs = 0.25, na.rm = TRUE),
+            q75 = quantile(cars_per_person, probs = 0.75, na.rm = TRUE),
+            min = min(cars_per_person, na.rm = TRUE),
+            max = max(cars_per_person, na.rm = TRUE),
+            sd = sd(cars_per_person, na.rm = TRUE))
+
+ggplot(all_summary_stop2, aes(x = year_offset, y = median, color = stop)) +
+  geom_vline(xintercept = 0) +
+  geom_line(size=1) +
+  geom_smooth(method=lm, se=FALSE, linetype="dotted") +
+  ylab("Median cars per person") +
+  xlab("Year's since station opended") +
+  guides(color=guide_legend(title="Station", ncol =1))
+
+ggsave("plots/change_stations_single.png", width = 210, height = 297, units = "mm")
+
+
+# class_summary <- classif %>% 
+#   group_by(stop) %>%
+#   summarise(carpp_change_max = max(carpp_max - carpp_min, na.rm = TRUE),
+#             carpp_change_min = min(carpp_max - carpp_min, na.rm = TRUE),
+#             carpp_change_sd = sd(carpp_max - carpp_min, na.rm = TRUE),
+#             carpp_change_mean = mean(carpp_max - carpp_min, na.rm = TRUE),
+#             carpp_change_median = median(carpp_max - carpp_min, na.rm = TRUE),
+#             carpp_percent_max = max((carpp_max - carpp_min) / carpp_max, na.rm = TRUE),
+#             carpp_percent_min = min((carpp_max - carpp_min) / carpp_max, na.rm = TRUE),
+#             carpp_percent_sd = sd((carpp_max - carpp_min) / carpp_max, na.rm = TRUE),
+#             carpp_percent_mean = mean((carpp_max - carpp_min) / carpp_max, na.rm = TRUE),
+#             carpp_percent_median = median((carpp_max - carpp_min) / carpp_max, na.rm = TRUE))
+# 
+# 
+# ggplot(class_summary, aes(x = year, y = median, color = SOAC11NM)) +
+#   geom_line(size=2) +
+#   ylab("Median cars per person") +
+#   xlab("Year") +
+#   guides(color=guide_legend(title="Area classification", ncol =1)) +
+#   scale_color_manual(values=cols)
+
+
+# Example Holt Town
+
+stop = "Shudehill Interchange"
+all_sub = all[all$code %in% c(bounds$geo_code[bounds$stop == stop]),]
+all_sub$intervention_year = stops$year[stops$stop == stop]
 all_sub$year_offset = all_sub$year - all_sub$intervention_year
 
-coeff =  max(c(all_sub$population), na.rm = T) / max(all_sub$cars_per_person, na.rm = T)
-ggplot(all_sub, aes(x = year_offset)) +
-  geom_line(aes(y = cars_per_person), size=2, color="red") +
-  geom_line(aes(y=population / coeff) , size=2, color="green") +
-  scale_y_continuous(
-    name = "Population",
-    sec.axis = sec_axis(~.*coeff, name="Population (green)")
-  )
+# coeff =  max(c(all_sub$population), na.rm = T) / max(all_sub$cars_per_person, na.rm = T)
+# ggplot(all_sub, aes(x = year_offset)) +
+#   geom_line(aes(y = cars_per_person), size=2, color="red") +
+#   geom_line(aes(y=population / coeff) , size=2, color="green") +
+#   geom_line(aes(y=AllCars / coeff) , size=2, color="blue") +
+#   scale_y_continuous(
+#     name = "Population",
+#     sec.axis = sec_axis(~.*coeff, name="Population (green)")
+#   )
+all_sub_long = tidyr::pivot_longer(all_sub[,c("code","year_offset","population","AllCars","cars_per_person")],
+                                   cols = c("population","AllCars","cars_per_person"))
+
+ggplot(all_sub_long, aes(year_offset, value, colour = code)) + geom_line() +
+  facet_wrap(~ name, ncol = 1, scales = "free_y")
+
+
 
 
 # Do for all data
